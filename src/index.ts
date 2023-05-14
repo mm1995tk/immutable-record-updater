2a51ab68cf93ab88a9adf2b688efcc65ecd529e6
mm1995tk <jimi.woodstock1995@gmail.com>
2023-05-15 07:44:42 +0900

    impl safe updater

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import type { FieldPath, FieldPathValue } from 'react-hook-form';
-import { Id, composeId, isNonEmptyArray } from './lib';
+import { Id, NonEmptyArray, composeId, isNonEmptyArray } from './lib';
 
 export { type Id } from './lib';
 
@@ -38,38 +38,130 @@ export type RecordUpdater<T extends FieldValues> = {
   ): RecordUpdater<T>;
 };
 
+export type SafeRecordUpdater<T extends FieldValues, Error> = {
+  /**
+   * it runs the program of update you define by recieve initial value.
+   */
+  run: (item: T) => Result<T, Error>;
+
+  /** it's an immutable setter. */
+  set<Path extends FieldPath<T>>(
+    /**
+     * path of value you would like to update.
+     */
+    path: Path,
+
+    /**
+     * value that replace old value or procedure of update.
+     */
+    valueOrFunc: ValueOrSafeFunc<T, Path, Error>
+  ): SafeRecordUpdater<T, Error>;
+
+  setIfNotNullish<Path extends FieldPath<T>>(
+    /**
+     * path of value you would like to update.
+     */
+    path: Path,
+
+    /**
+     * value that replace old value or procedure of update.
+     */
+    valueOrFunc:
+      | Value<T, Path>
+      | ((item: NonNullable<FieldPathValue<T, Path>>, origin: () => Result<T, Error>) => FieldPathValue<T, Path>)
+  ): SafeRecordUpdater<T, Error>;
+};
+
 /**
  * generate updater.
  * @param constraints - constraints record should fulfill. you also can use this as preprocessor.
  */
-export const generateRecordUpdater = <T extends FieldValues>() =>
-  // ...constraints: Id<T>[]
-  {
-    const updater = (queue: Id<T>[]): RecordUpdater<T> => {
-      const createSetter =
-        (b: boolean) =>
-        <Path extends FieldPath<T>>(path: Path, valueOrFunc: ValueOrFunc<T, Path>) => {
-          return updater([
-            ...queue,
-            origin => {
-              const go = getGo(origin, valueOrFunc, b);
+export const generateRecordUpdater = <T extends FieldValues>() => {
+  const updater = (queue: Id<T>[]): RecordUpdater<T> => {
+    const createSetter =
+      (b: boolean) =>
+      <Path extends FieldPath<T>>(path: Path, valueOrFunc: ValueOrFunc<T, Path>) => {
+        return updater([
+          ...queue,
+          origin => {
+            const go = getGo(origin, valueOrFunc, b);
 
-              return go(origin, path.split('.'));
-            },
-          ]);
-        };
-
-      return {
-        run: origin => {
-          return isNonEmptyArray(queue) ? composeId(...queue)(origin) : origin;
-        },
-        set: createSetter(false),
-        setIfNotNullish: createSetter(true),
+            return go(origin, path.split('.'));
+          },
+        ]);
       };
-    };
 
-    return updater([]);
+    return {
+      run: origin => {
+        return isNonEmptyArray(queue) ? composeId(...queue)(origin) : origin;
+      },
+      set: createSetter(false),
+      setIfNotNullish: createSetter(true),
+    };
   };
+
+  return updater([]);
+};
+
+export const generateSafeRecordUpdater = <T extends FieldValues, Error extends DefaultError | string = DefaultError>(
+  ...constraints: Constraint<T, Error>[]
+) => {
+  const validate: Constraint<T, Error> = pre => {
+    if (!isNonEmptyArray(constraints)) {
+      return { success: true };
+    }
+
+    let acc = constraints[0](pre);
+
+    for (const cur of constraints.slice(1)) {
+      if (!acc.success) {
+        break;
+      }
+      acc = cur(pre);
+    }
+
+    return acc;
+  };
+
+  const updater = (queue: Id<T>[]): SafeRecordUpdater<T, Error> => {
+    const createSetter =
+      (b: boolean) =>
+      <Path extends FieldPath<T>>(path: Path, valueOrFunc: ValueOrSafeFunc<T, Path, Error>) => {
+        return updater([
+          ...queue,
+          origin => {
+            const go = getSafeGo(origin, valueOrFunc, b, validate);
+
+            return go(origin, path.split('.'));
+          },
+        ]);
+      };
+
+    return {
+      run: origin => {
+        const result = isNonEmptyArray(queue) ? composeId(...queue)(origin) : origin;
+        const validated = validate(result);
+        if (!validated.success) {
+          return validated;
+        }
+        return { success: true, data: result };
+      },
+      set: createSetter(false),
+      setIfNotNullish: createSetter(true),
+    };
+  };
+
+  return updater([]);
+};
+
+export const joinByDot =
+  <T extends string>(item: T) =>
+  <U extends string | number>(n: U): `${T}.${U}` =>
+    `${item}.${n}`;
+
+export default { generateRecordUpdater, joinByDot, generateSafeRecordUpdater };
+
+// ########################################################################################################################
 
 const getGo = <T extends FieldValues, Path extends FieldPath<T>>(
   origin: T,
@@ -103,18 +195,52 @@ const getGo = <T extends FieldValues, Path extends FieldPath<T>>(
   return go;
 };
 
+const getSafeGo = <T extends FieldValues, Path extends FieldPath<T>, Error>(
+  origin: T,
+  valueOrFunc: ValueOrSafeFunc<T, Path, Error>,
+  nullishFlag: boolean,
+  validate: Constraint<T, Error>
+) => {
+  const go = (item: any, keys: string[]): any => {
+    if (item == null && !!keys.length) {
+      return null;
+    }
+
+    if (!keys.length) {
+      if (!isSafeFunc(valueOrFunc)) {
+        return valueOrFunc;
+      }
+
+      if (nullishFlag && item == null) {
+        return item;
+      }
+
+      return valueOrFunc(item, () => {
+        const validated = validate(origin);
+        if (!validated.success) {
+          return validated;
+        }
+        return { success: true, data: result };
+      });
+    }
+
+    const key = keys[0];
+    const next = go(item[key], keys.slice(1));
+    const result = { ...item, [key]: next };
+
+    return Array.isArray(item) ? Object.values(result) : result;
+  };
+
+  return go;
+};
+
 const isFunc = <T extends FieldValues, Path extends FieldPath<T>>(
   valueOrFunc: ValueOrFunc<T, Path>
 ): valueOrFunc is Func<T, Path> => typeof valueOrFunc === 'function';
 
-export const joinByDot =
-  <T extends string>(item: T) =>
-  <U extends string | number>(n: U): `${T}.${U}` =>
-    `${item}.${n}`;
-
-export default { generateRecordUpdater, joinByDot };
-
-// ########################################################################################################################
+const isSafeFunc = <T extends FieldValues, Path extends FieldPath<T>, Error>(
+  valueOrFunc: ValueOrSafeFunc<T, Path, Error>
+): valueOrFunc is SafeFunc<T, Path, Error> => typeof valueOrFunc === 'function';
 
 type FieldValues = Record<string, unknown>;
 
@@ -126,3 +252,27 @@ type Func<T extends FieldValues, Path extends FieldPath<T>> = (
   item: FieldPathValue<T, Path>,
   origin: T
 ) => FieldPathValue<T, Path>;
+
+type ValueOrSafeFunc<T extends FieldValues, Path extends FieldPath<T>, Error> =
+  | Value<T, Path>
+  | SafeFunc<T, Path, Error>;
+
+type SafeFunc<T extends FieldValues, Path extends FieldPath<T>, Error> = (
+  item: FieldPathValue<T, Path>,
+  origin: () => Result<T, Error>
+) => FieldPathValue<T, Path>;
+
+type Constraint<T extends FieldValues, Error> = (item: T) =>
+  | {
+      success: true;
+    }
+  | { success: false; error: Error };
+
+type Result<T extends FieldValues, Error> =
+  | {
+      success: true;
+      data: T;
+    }
+  | { success: false; error: Error };
+
+type DefaultError = Error;
